9932c06b3735dd0edf278ab72864204fee45a7c0
Vrajesh Patel <u18102672@tuks.co.za>
2020-08-05 09:08:11 +0200

    Added Admin Controller and 1 view

---
+++ INF272 Deliverable 2/INF272 Deliverable 2/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace INF272_Deliverable_2.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult AdminHome()
+        {
+            return View();
+        }
+    }
+}
